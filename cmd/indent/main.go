3647b58e03f9d93f69570eca5f25988a5ff084d6
Wojciech Kaczmarek <wojtekk@kofeina.net>
2025-03-12 23:22:55 +0100

    cmd: FILE arg; proper flags loop

--- cmd/indent/main.go
+++ cmd/indent/main.go
@@ -9,41 +9,77 @@ import (
 )
 
 type pargs struct {
+	file string
 	help func()
 }
 
-func parseArgs(args []string) (p pargs, _ error) {
-	const usage = `usage: indent  (reads stdin, outputs to stdout)`
-	if len(args) > 0 {
-		if len(args) == 1 {
-			switch arg := args[0]; {
-			case arg == "-h", arg == "--help":
-				p.help = func() { fmt.Println(usage) }
-				return p, nil
-			}
+func parseArgs(args []string) (a pargs, _ error) {
+	const usage = `usage: indent [FILE|-]  - reads FILE or stdin, prints to stdout`
+
+	var rest []string
+flags:
+	for ; len(args) > 0; args = args[1:] {
+		switch arg := args[0]; {
+
+		case arg == "-h", arg == "--help":
+			a.help = func() { fmt.Println(usage) }
+			return a, nil
+
+		case arg == "--":
+			rest = append(rest, args[1:]...)
+			break flags
+
+		case len(arg) > 1 && arg[0] == '-':
+			return a, fmt.Errorf("unknown flag %s\n%s", arg, usage)
+
+		default:
+			rest = append(rest, arg)
+			continue
 		}
-		return p, fmt.Errorf(usage)
 	}
-	return p, nil
+
+	switch len(rest) {
+	case 0:
+		a.file = "-"
+	case 1:
+		a.file = rest[0]
+	case 2:
+		return a, fmt.Errorf("too many file args\n%s", usage)
+	}
+
+	return a, nil
 }
 
-func run() error {
-	r := indent.NewReader(os.Stdin, "\t")
-	_, err := io.Copy(os.Stdout, r)
+func open(file string) (*os.File, error) {
+	if file == "-" {
+		return os.Stdin, nil
+	}
+	return os.Open(file)
+}
+
+func run(a *pargs) error {
+	f, err := open(a.file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	r := indent.NewReader(f, "\t")
+	_, err = io.Copy(os.Stdout, r)
 	return err
 }
 
 func main() {
-	p, err := parseArgs(os.Args[1:])
+	a, err := parseArgs(os.Args[1:])
 	if err != nil {
 		die(2, err)
 	}
-	if p.help != nil {
-		p.help()
+	if a.help != nil {
+		a.help()
 		os.Exit(0)
 	}
 
-	err = run()
+	err = run(&a)
 	if err != nil {
 		die(1, err)
 	}
